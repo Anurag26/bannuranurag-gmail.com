885e77305eccd887575d05fc487d839dc7fc680d
bannur <bannur.a@husky.neu.edu>
2020-03-24 20:27:47 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import {FormsModule} from '@angular/forms';
+import {CourseServiceClient} from './services/CourseServiceCLient';
+import { CourseViewerComponentComponent } from './course-viewer-component/course-viewer-component.component';
+import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseViewerComponentComponent,
+    ModuleListComponentComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+  providers: [
+    CourseServiceClient
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
